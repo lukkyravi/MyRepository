4e7f0252f344393dfa20070e15e94a937816371d
lukkyravi <lukkyravi1@gmail.com>
2017-04-05 12:51:31 -0400

    Add files via upload

---
+++ AdditionQuiz.py
@@ -0,0 +1,14 @@
+#AdditionQuiz.py
+import random
+
+#Generate random numbers
+
+number1 = random.randint(0,9)
+number2 = random.randint(0,9)
+
+#Prompt the user to Enter an answer
+
+answer =  eval(input("What is " + str(number1) + str(number2) + "? "))
+
+#Display result
+print(number1, "+", number2, "=" answer , "is", number1 + number2 == answer)
